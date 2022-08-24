df43898524928066bddac799e66ce149d46c2527
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-08-24 12:20:13 +1000

    Initial commit

---
+++ src/test/java/pro/sky/homework_collections/HomeworkCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
